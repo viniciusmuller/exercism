c0b98975fef29eb2fef37521652e7cdfbebf5d10
arcticlimer <vinigm.nho@gmail.com>
2020-12-17 20:25:59 -0300

    Improve solution

--- elixir/rna-transcription/lib/rna_transcription.ex
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -9,20 +9,16 @@ defmodule RnaTranscription do
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-    # I think this is more efficient, i don't know exactly how the garbage
-    # collector performs in this situation, but i think this is doing just the
-    # same as a single Enum.map, creating a new list each iteration.
-    Enum.reduce(dna, [], fn char, accumulator ->
-       accumulator ++ do_rna(char)
-    end)
+    dna
+    |> Enum.map(&do_rna/1)
   end
 
   defp do_rna(c) do
     case c do
-      ?G -> 'C'
-      ?C -> 'G'
-      ?T -> 'A'
-      ?A -> 'U'
+      ?G -> ?C
+      ?C -> ?G
+      ?T -> ?A
+      ?A -> ?U
     end
   end
 end
