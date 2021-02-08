ac25f7f3911938f17c5efaeecaf33e2d36edc13d
arcticlimer <vinigm.nho@gmail.com>
2021-02-07 21:34:07 -0300

    Complete challenge

--- erlang/raindrops/src/raindrops.erl
+++ erlang/raindrops/src/raindrops.erl
@@ -4,17 +4,26 @@
 
 
 convert(Number) ->
-  Items = [{3, "Pling"}, {5, "Plang"}, {7, "Plong"}]
+  Items = [{3, "Pling"}, {5, "Plang"}, {7, "Plong"}],
 
-  Result = lists:foldl(fun ({Item, Sound}, Acc) ->
-      if Number rem Item =:= 0 -> Acc ++ Sound;
-      true -> Acc
-      end
-    end
-  , "", Items)
+  Result = lists:foldl(
+    fun (Tuple, Acc) ->
+      make_sound(Tuple, Acc, Number) end,
+    "",
+    Items
+  ),
 
   case Result of
-    [] -> integer_to_list(Number)
+    [] -> integer_to_list(Number);
     _ -> Result
   end
+.
+
+make_sound({Item, Sound}, Acc, Number)
+  when Number rem Item =:= 0
+  ->
+    Acc ++ Sound
+;
+make_sound(_, Acc, _) ->
+  Acc
 .
